093ef276c7c073103d069253abbed2fd549262d1
AnnMary-103 <annmarybinu00@gmail.com>
2024-02-05 15:24:40 +0530

    commit

--- NexusEduTech_BackEnd/Models/Admin.cs
+++ NexusEduTech_BackEnd/Models/Admin.cs
@@ -1,15 +1,62 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NexusEduTech_BackEnd.Models
 {
-    public class Admin
-    {
-    }
+   
 
-    [Table("User")]
-    public class Add_User
+    [Table("tbl_CustomUser")]
+    public class CustomUser
     {
 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("UserId", TypeName = "int")]
+        public int UserId { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Valid User Name")]
+        [Column("UserName", TypeName = "varchar")]
+        [StringLength(50)]
+        public string UserName { set; get; }
+
+        [Required(ErrorMessage = "Enter Valid First Name")]
+        [Column("FName", TypeName = "varchar")]
+        [StringLength(50)]
+        public string FName { set; get; }
+
+        [Required(ErrorMessage = "Enter Valid Last Name")]
+        [Column("LName", TypeName = "varchar")]
+        [StringLength(50)]
+        public string LName { set; get; }
+
+        [Required(ErrorMessage = "Enter Valid Gender")]
+        [Column("Gender", TypeName = "varchar")]
+        [StringLength(10)]
+        public string Sex { set; get; }
+
+        [Required(ErrorMessage = "Enter Valid DOB")]
+        public DateTime DOB { set; get; }
+
+        [Required(ErrorMessage = "Enter Valid Address")]
+        [Column("Address", TypeName = "varchar")]
+        [StringLength(50)]
+        public string Address { set; get; }
+
+        [Required(ErrorMessage = "Enter Email")]
+        [EmailAddress(ErrorMessage = "Invalid EmailId")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter Valid Role")]
+        [Column("Role", TypeName = "varchar")]
+        [StringLength(10)]
+        public string Role { set; get; }
+
+        [Required(ErrorMessage = "Enter IsActive ")]
+        [Column("IsActive")]
+        public bool IsActive { set; get; }
+
     }
+
 
 }
--- NexusEduTech_BackEnd/Models/Student.cs
+++ NexusEduTech_BackEnd/Models/Student.cs
@@ -1,7 +1,25 @@
-﻿namespace NexusEduTech_BackEnd.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace NexusEduTech_BackEnd.Models
 {
-    
+   
     public class Student
     {
+        [Required]
+        public int StudentId { get; set; }
+
+        [ForeignKey("UserId")]
+        public CustomUser? UserId { get; set; }
+
+        [Required]
+        public int ClassRoomId { get; set; }
+
+       // [ForeignKey("ClassRoomId")]
+       // public Classes? ClassRoomId { get; set; }
+       
+        [Required]
+        [Column("Registration Number",TypeName ="varchar")]
+        [StringLength(50)]
+        public string RegistrationNumber { get; set; }
     }
 }
